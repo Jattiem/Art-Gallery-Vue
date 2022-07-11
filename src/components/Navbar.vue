03bf02466c31d19dc9a689d4b3cc7020dc8105be
jattiemyaseen <yaseenjattiem0000@gmail.com>
2022-07-11 09:19:01 +0200

    busy

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,3 +8,22 @@
     <router-link to="/admin">Admin</router-link>
   </nav>
 </template>
+<script>
+export default {
+
+}
+</script>
+<style>
+    nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
